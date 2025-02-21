7da86bc4f94834dd5916361684821d2d95138f53
thuliovs <thuliowork@gmail.com>
2025-02-21 17:25:21 +0000

    Update

--- src/writer/views.py
+++ src/writer/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 from common.auth import awriter_required # type: ignore
-
+from common.django_utils import arender
 
 @awriter_required
 async def dashboard(request: HttpRequest) -> HttpResponse:
-    return render(request, 'writer/dashboard.html')
+    return await arender(request, 'writer/dashboard.html')
